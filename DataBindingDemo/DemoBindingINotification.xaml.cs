fea078c2f9bed031e414573ea2a9ac021a358c17
Ofer Zadikario <ofer.zadikario@gmail.com>
2021-03-10 09:33:37 +0200

    Add project files.

---
+++ DataBindingDemo/DemoBindingINotification.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DemoBindingINotification : ContentPage
+    {
+        private StudentWithNotification ReadStudentInfo()
+        {
+            return new StudentWithNotification()
+            {
+                Name = "Ofer",
+                Grade = 70
+            };
+        }
+        public DemoBindingINotification()
+        {
+            this.BindingContext = ReadStudentInfo();
+            InitializeComponent();
+        }
+
+        private void btnBonus_Clicked(object sender, EventArgs e)
+        {
+            StudentWithNotification student = (StudentWithNotification)this.BindingContext;
+            student.Grade += 20;
+        }
+    }
+}
---
+++ DataBindingDemo/Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataBindingDemo
+{
+    class Student
+    {
+        private string name;
+        private int grade;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                    this.name = value;
+            }
+        }
+        public int Grade
+        {
+            get
+            {
+                return this.grade;
+            }
+            set
+            {
+                this.grade = value;
+            }
+        }
+    }
+
+    class StudentWithNotification: INotifyPropertyChanged
+    {
+        private string name;
+        private int grade;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Name 
+        { 
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }    
+        public int Grade 
+        {
+            get
+            {
+                return this.grade;
+            }
+            set
+            {
+                if (this.grade != value)
+                {
+                    this.grade = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
